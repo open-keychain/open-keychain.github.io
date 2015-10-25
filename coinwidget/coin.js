96e338baf45897ad11e8c2a4a323a8c4dbb43452
Dominik Schürmann <dominik@dominikschuermann.de>
2015-10-25 21:22:30 +0100

    Host coinwidget

--- coinwidget/coin.js
+++ coinwidget/coin.js
@@ -37,7 +37,7 @@ var CoinWidgetComCounter = 0;
 
 if (typeof CoinWidgetCom != 'object')
 var CoinWidgetCom = {
-	source: 'http://coinwidget.com/widget/'
+	source: 'https://www.openkeychain.org/coinwidget/'
 	, config: []
 	, go :function(config) {
 		config = CoinWidgetCom.validate(config);
