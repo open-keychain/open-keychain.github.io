80fc6ad27499108dbe155ebb4f1ff990cd9872ab
Dominik Schürmann <dominik@dominikschuermann.de>
2015-12-09 00:35:48 +0100

    no toc effect

--- public/js/toc/toc.js
+++ public/js/toc/toc.js
@@ -7,7 +7,7 @@
       minimumHeaders: 3,
       headers: 'h1, h2, h3, h4, h5, h6',
       listType: 'ol', // values: [ol|ul]
-      showEffect: 'none', // values: [show|slideDown|fadeIn|none]
+      showEffect: 'show', // values: [show|slideDown|fadeIn|none]
       showSpeed: 'slow' // set to 0 to deactivate effect
     },
     settings = $.extend(defaults, options);
